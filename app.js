4946f1c71c7f3cd68f7696aa9f84291a38c1e48d
ishrat-educative <88875358+ishrat-educative@users.noreply.github.com>
2022-01-31 12:17:55 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const port = process.env.PORT || "8080";
-const message = "BYE_999999999" + process.env.TARGET || "World!!";
+const message = "YYYYTTTTT" + process.env.TARGET || "World!!";
 
 app.get("/", (req, res) => res.send(message));
 server.listen(port, function () {
